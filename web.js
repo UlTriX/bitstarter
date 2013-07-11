b41c7601566f7f0e820641ff2578b75309fea302
Ubuntu <ubuntu@ip-172-31-12-238.us-west-2.compute.internal>
2013-07-11 19:45:55 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
-Var Port = Process.env.PORT || 5000;
+var port = Process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
